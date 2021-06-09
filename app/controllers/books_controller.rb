d09fd7f9f3c9dfc4f70a20173e0e706e830df6d4
tatuka114 <ec2-user@ip-172-31-71-251.ec2.internal>
2021-06-09 16:14:24 +0000

    [Update]4-10 bootstrapを追加する

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,8 +5,11 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
-    @book.save
-    redirect_to books_path
+    if @book.save
+      redirect_to book_path(@book), notice:"You have created book successfully."
+    else
+      render :index
+    end
   end
   def index
     @book = Book.new
@@ -22,6 +25,6 @@ class BooksController < ApplicationController
   end
   private
   def book_params
-    params.require(:book).permit(:profile_image,:title,:opinion)
+    params.require(:book).permit(:profile_image, :title, :opinion)
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
+    @users = Book.all
   end
   def edit
     @user = User.find(params[:id])
