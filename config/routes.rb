d8770d3eeb02a98e0540b9ec1260f8fd298235a9
tatuka114 <ec2-user@ip-172-31-71-251.ec2.internal>
2021-06-13 09:33:01 +0000

    [Update]4-10 header

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   root to: "homes#top"
   resources :books, only: [:new, :create, :index, :show, :destroy]
   resources :users, only: [:show, :edit, :update, :index]
+  get "/home/about" => "homes#about"
 end
