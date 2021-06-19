fadb40821357b676f7de0598b1d1b0e7d7713986
tatuka114 <ec2-user@ip-172-31-71-251.ec2.internal>
2021-06-19 19:52:52 +0000

    [Update]4-10 error3

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -33,6 +33,7 @@ class BooksController < ApplicationController
       @book = Book.find(params[:id])
       @book.user_id = current_user.id
       if @book.update(book_params)
+        flash[:notice] = "You have updated user successfully."
         redirect_to book_path(@book.id)
       else
         render :edit
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,7 +23,8 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
     if @user.update(user_params)
-     redirect_to user_path(current_user)
+      flash[:notice] = "You have updated user successfully."
+      redirect_to user_path(current_user)
     else
       render :edit
     end
